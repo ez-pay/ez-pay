5c46e57cd14da34ed167545898cb99d99b80b333
wjacobw <williamjacob0910@gmail.com>
2024-09-21 14:51:56 +0800

    trying

--- app/layout.tsx
+++ app/layout.tsx
@@ -90,7 +90,7 @@ export default function RootLayout({ children }: React.PropsWithChildren) {
           <main className="mx-auto w-full max-w-3xl flex-auto px-4 py-4 sm:px-6 md:py-6">
             {children}
           </main> */}
-          <AuthPage />
+          {/* <AuthPage /> */}
           {/* <LoginPage /> */}
         </div>
       </body>
